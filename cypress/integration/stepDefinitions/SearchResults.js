ef2c27dbeafb2ff0a53e3e0df24d955bfa71952d
Kshitij Kumar <kshitij.kumar1@3pillarglobal.com>
2023-04-27 18:43:40 +0530

    Locators added to fixture folder

--- cypress/integration/stepDefinitions/SearchResults.js
+++ cypress/integration/stepDefinitions/SearchResults.js
@@ -1,40 +1,41 @@
 /// <reference types="Cypress" />
 import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
+var locator, data
 Given("I navigate to the website",()=>{
     cy.openAmbetterGuide();
-    // cy.fixture('locators').then((locator)=>{
-    //     this.locator=locator
-    // })
-    // cy.fixture('data').then((data)=>{
-    //     this.data=data
+    cy.fixture('locators.json').then((locators)=>{
+        locator=locators
+    })
+    // cy.fixture('data.json').then((d)=>{
+    //     data=d
     // })
 })
 
 When("I choose home state option",()=>{
     cy.wait(7000)
-    cy.get('div[id="main-content"]').children().children().next().children().next().children().next().children().next().next().children().children().first().click()
+    cy.get(locator.searchResults.homeState).children().children().next().children().next().children().next().children().next().next().children().children().first().click()
 })
 When("Select state and coverage year and click on continue button",()=>{
-    cy.get('div[id="main-content"]').children().children().next().children().children().next().children().children().children().children().first().type("New")
+    cy.get(locator.searchResults.homeState).children().children().next().children().children().next().children().children().children().children().first().type("New")
     cy.wait(5000)
-    cy.selectFromDropDown('div[data-testid="autocomplete-menu"]','New Jersey')
+    cy.selectFromDropDown(locator.searchResults.stateDropDown,'New Jersey')
     cy.wait(5000)
-    cy.get('button[type="submit"]').click()
+    cy.get(locator.searchResults.submitButton).click()
 })
 And("Select a network and click on start browsing button",()=>{
-    cy.get('button[type="submit"]').click()
+    cy.get(locator.searchResults.submitButton).click()
 })
 And("Search doctors using procedure and city and click on Search button",()=>{
-    cy.get('input[placeholder="Start searching here..."]').type("Primary")
-    cy.get('li[data-testid="autoCompleteMenuItem"]').each(($ele, index, $list)=>{
+    cy.get(locator.searchResults.procedureLabel).type("Primary")
+    cy.get(locator.searchResults.procedureDropDown).each(($ele, index, $list)=>{
         if($ele.text()==="Primary Care Provider"){
             cy.wrap($ele).click()
         }
     })
-    cy.get('input[placeholder="Location"]').clear()
-    cy.get('input[placeholder="Location"]').type("New")
-    cy.selectFromDropDown('div[class="pac-item"]','New York')
-    cy.get('button[data-testid="search-button"]').click()
+    cy.get(locator.searchResults.locationLabel).clear()
+    cy.get(locator.searchResults.locationLabel).type("New")
+    cy.selectFromDropDown(locator.searchResults.cityDropDown,'New York')
+    cy.get(locator.searchResults.searchButton).click()
     cy.wait(8000)
 })
 And("Healthcare providers should be visible",()=>{
@@ -46,31 +47,31 @@ And("Healthcare providers should be visible",()=>{
 })
 
 When("I click on a healthcare provider and details page open",()=>{
-    cy.get('div[data-testid="searchResults"]').children().next().children().children().next().children().next().children().children().eq(0).click()
+    cy.get(locator.searchResults.detailPageButton).children().next().children().children().next().children().next().children().children().eq(0).click()
 })
 var NPI, Provider
 And("I copy NPI number",()=>{
     cy.wait(2000)
-    cy.get('div[data-copytype="npi"]').invoke('text').as('Npi')
+    cy.get(locator.searchResults.npiNumber).invoke('text').as('Npi')
     cy.get('@Npi').then((NpiNum)=>{
         NPI=NpiNum
     })
-    cy.get('h1[data-testid="signature"]').invoke('text').as('ProviderName')
+    cy.get(locator.searchResults.providerName).invoke('text').as('ProviderName')
     cy.get('@ProviderName').then((Name)=>{
         Provider=Name
     })
 })
 And("Click on Advanced Search tab",()=>{
-    cy.get('a[href="/advanced-search"]').click()
+    cy.get(locator.searchResults.advancedSearchTab).click()
 })
 And("Paste the NPI number in the label",()=>{
-    cy.get('input[data-testid="advancedKeywordInput"]').type(NPI.substring(5,15))
+    cy.get(locator.searchResults.npiNumberLabel).type(NPI.substring(5,15))
 })
 And("Click on Submit button",()=>{
     cy.contains("Submit").click()
     cy.wait(8000)
 })
 Then("The same healthcare provider must be available in search Results",()=>{
-    cy.verifyText('span[data-testid="signature"]',Provider)
+    cy.verifyText(locator.searchResults.providerNameVerification,Provider)
 })
 
